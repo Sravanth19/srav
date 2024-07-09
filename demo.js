ea77671c2b9dc18b502b4f6965aee71deb76c7bf
Sravanth19 <224g1a33a5@srit.ac.in>
2024-07-09 09:29:29 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,19 @@
+console.log("hello sravanth and how is manasa")
+console.table({a:5 , b:10})
+//console.warn , console.error
+
+//In variable we use _num , $num.
+ // in 'var' re assign and re declare is possible
+ // in 'let' re assign is possible but redeclare is  not possible.
+ // in 'const' re assign and re declare is not  possible 
+ 
+ /* Data types*/
+ // array -[]
+ //objects -{}
+ //Null - null
+ //undefined - undefined
+// prompt and alert and confirm
+
+ /* Type conversion ..(typeof) */
+ // null - ie , a=null 
+ // undefined - ie , a=
